68ccfed0f2896649a0d83f2f2465569a5ad716ec
Thomas Beverley <tom.beverley101@gmail.com>
2015-10-15 12:39:36 +0100

    Updated the publish plugin to use post ids rather than indexes are these are more consistent. Also added extra functionality to enable live reloading when posts change

--- stacktodo/slack_publish.js
+++ stacktodo/slack_publish.js
@@ -16,23 +16,103 @@
 				default: throw('InitializationError : Incorrect params');
 			}
 		})(Array.prototype.slice.call(arguments));
-		var index = null;
+		var postsIndex = null;
 		var posts = {};
 
-		self.loaded = function() { return index !== null; };
-		self.count = function() { return index === null ? undefined : index.length; };
-		self.urlAt = function(i) { return index === null ? undefined : index[i]; };
+		/**
+		* @GETTER
+		* @return true if the posts index has been loaded
+		*/
+		self.loaded = function() { return postsIndex !== null; };
+
+		/**
+		* @GETTER
+		* @return the amount of posts in the index
+		*/
+		self.count = function() { return postsIndex === null ? undefined : postsIndex.length; };
+
+		/**
+		* @GETTER
+		* @param postId: the id of the post to get
+		* @return the post head or undefined if not found
+		*/
+		self.postHeadForId = function(postId) {
+			for (var i = 0; i < (postsIndex || []).length; i++) {
+				if (postsIndex[i].id === postId) { return postsIndex[i]; }
+			}
+			return undefined;
+		};
+
+		/**
+		* @GETTER
+		* @param index: the index for the post
+		* @return the post head or undefined if not found
+		*/
+		self.postHeadForIndex = function(index) { return postsIndex === null ? undefined : postsIndex[i]; };
+
+		/**
+		* @GETTER
+		* @param postId: the id of the post
+		* @return true or false if this id is the last post
+		*/
+		self.isPostIdLast = function(postId) {
+			if (postsIndex === null) { return false; }
+			if (postsIndex.length === 0) { return false; }
+			return postsIndex[postsIndex.length - 1].id === postId;
+		};
+
+		/**
+		* @GETTER
+		* @param lastPostId=null: the id of the previous post that you want to start from
+		* @param count: the number of items to get
+		* @return a list of posts to fetch or undefiend if the posts index is not loaded
+		*/
+		self.nextPostHeadsForRange = function(lastPostId, count) {
+			if (postsIndex === null) { return undefined; }
+
+			// Get the start
+			var start = 0;
+			if (lastPostId) {
+				for (var i = 0; i < postsIndex.length; i++) {
+					if (postsIndex[i].id === lastPostId) {
+						start = i + 1;
+						break;
+					}
+				}
+			}
+			start = Math.min(Math.max(0, start), self.count());
+
+			// Get the end
+			var end = Math.min(Math.max(0, start + count), self.count());
+
+			// Get the list of posts
+			var items = [];
+			for (var i = start; i < end; i++) {
+				items.push(postsIndex[i]);
+			}
+
+			return items;
+		};
+
+
+
+		
 
 		/**
 		* Loads the blog index from stacktodo
 		* @param success=undefined: executed on success
 		* @param failure=undefined: executed on failure, given the http status and error
+		* @return self
 		*/
 		self.load = function(success, failure) {
 			ns.stacktodo.core.jsoncors.get({
 				url 			: 'https://stacktodo.com/api/tool/publish/posts' + indexUrlQuery,
 				success			: function(response, status, xhr) {
-					index = response.posts;
+					postsIndex = response.posts;
+					// Enhance the posts slightly
+					for (var i = 0; i < postsIndex.length; i++) {
+						postsIndex[i].index = i;
+					}
 					if (success) { success(); }
 				},
 				failure 		: function(status, error, xhr) {
@@ -44,35 +124,51 @@
 
 		/**
 		* Loads a post at the given index
-		* @param index: the index of the post to load
+		* @param postId: the id of the post to load
 		* @param success=undefined: executed on load success. Provided with the html
 		* @param failure=undefined: executed on load failure
+		* @return self
 		*/
-		self.post = function(index, success, failure) {
+		self.postForIndex = function(index, success, failure) {
+			var head = self.postHeadForIndex(index);
+			return self.postForId((head || {}).id, success, failure);
+		};
+
+		/**
+		* Loads a post at the given index
+		* @param postId: the id of the post to load
+		* @param success=undefined: executed on load success. Provided with the html
+		* @param failure=undefined: executed on load failure
+		* @return self
+		*/
+		self.postForId = function(postId, success, failure) {
 			success = success || function() {};
 			failure = failure || function() {};
 
-			if (posts[index]) {
-				setTimeout(function() { success(posts[index]); }, 1);
+			if (posts[postId]) {
+				setTimeout(function() { success(posts[postId]); }, 1);
 			} else {
-				var url = self.urlAt(index);
-				if (url === undefined) {
+				var head = self.postHeadForId(postId);
+				if (head === undefined) {
 					setTimeout(function() { failure(-1, 'IndexNotLoadedError'); });
 				} else {
 					ns.stacktodo.core.ajax.get({
-						url 		: url + '?ts=' + new Date().getTime(),
+						url 		: head.url + '?ts=' + new Date().getTime(),
 						success 	: function(response, status, xhr) {
-							// Parse and sanitize the response
 							try {
 								response = JSON.parse(response);
 							} catch(ex) {
 								failure(-1, 'JSONParseError');
 								return;
 							}
+							// Enhance the post
 							response.html = response.html.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, '');
+							response.id = head.id;
+							response.url = head.url;
+							response.index = head.index;
 
 							// Save and respond
-							posts[index] = response;
+							posts[postId] = response;
 							success(response);
 						},
 						failure 	: function(status, error, xhr) {
@@ -86,56 +182,72 @@
 
 		/**
 		* Loads a set of posts. Silently validates if the start or end falls outside the bounds of posts
-		* @param startIndex: the index of the first post to load
-		* @param endIndex: the index of the last post to load
+		* @param lastId=null: the id of the last post. This will load the next one
+		* @param count: the number of posts to load
 		* @param success=undefined: executed on load success. Provided with a list of html strings
 		* @param failure=undefined: executed on load failure
+		* @return self
 		*/
-		self.postset = function(startIndex, endIndex, success, failure) {
+		self.postset = function(lastId, count, success, failure) {
 			success = success || function() {};
 			failure = failure || function() {};
-			startIndex = Math.min(Math.max(0, startIndex), self.count());
-			endIndex = Math.min(Math.max(0, endIndex), self.count());
 
-			// Check we have something to do
-			if (startIndex === 0 && endIndex === 0) {
-				success({}); return;
-			}
-			if (startIndex === self.count() && endIndex === self.count()) {
-				success({}); return;
-			}
+			var heads = self.nextPostHeadsForRange(lastId, count);
+			if (heads === undefined) {
+				setTimeout(function() { failure(-1, 'IndexNotLoadedError'); });
+			} else if (heads.length === 0) {
+				setTimeout(function() { success({}); });
+			} else {
+				// Setup our return map
+				var hasError = false;
+				var requests = {};
+				for (var i = 0; i < heads.length; i++) {
+					requests[heads[i].id] = null;
+				}
 
-			// Setup our return map
-			var hasError = false;
-			var requests = {};
-			for (var i = startIndex; i < endIndex; i++) {
-				requests[i] = null;
-			}
+				// Fire all the requests off (the browser will limit the quantity we are able to make here)
+				for (var i = 0; i < heads.length; i++) {
+					(function(id, url) {
+						self.postForId(id, function(post) {
+							if (hasError) { return; }
+							requests[id] = post;
 
-			// Fire all the requests off (the browser will limit the quantity we are able to make here)
-			for (var i = startIndex; i < endIndex; i++) {
-				(function(i) {
-					self.post(i, function(html) {
-						if (hasError) { return; }
-						requests[i] = html;
-						var hasWaiting = false;
-						for (var k in requests) {
-							if (requests[k] === null) {
-								hasWaiting = true;
-								break;
+							for (var k in requests) {
+								if (requests[k] === null) { return; }
 							}
-						}
-						if (!hasWaiting) {
+
+							// Catch the final one here
 							success(requests);
-						}
-					}, function(status, error) {
-						if (hasError) { return; }
-						hasError = true;
-						failure(status, error);
-					});
-				})(i);
-			}			
+						}, function(status, error) {
+							if (hasError) { return; }
+							hasError = true;
+							failure(status, error);
+						});
+					})(heads[i].id, heads[i].url);
+				}
+			}
+
+			return self;
 		};
+
+		/**
+		* Invalidates a post so it will be refetched from the server next time it's called
+		* @param postId: the id of the post to invalidate
+		* @return self
+		*/
+		self.invalidatePost = function(postId) { delete posts[postId]; return self; };
+
+		/**
+		* Invalidates all the posts so they must be refetched from the server
+		* @return self
+		*/
+		self.invalidatePosts = function() { posts = {}; return self; }
+
+		/**
+		* Invalidates the index so it must be refetched from the server
+		* @return self
+		*/
+		self.invalidateIndex = function() { postsIndex = null; return self; };
 
 		return self;
 	};
